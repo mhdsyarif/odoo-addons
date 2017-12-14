8d57b076e8eb4ba9afd7edbe843626b573811a7b
mhdsyarif <mhdsyarif.ms@gmail.com>
2017-12-14 20:40:15 +0700

    [ADD] First commit

---
+++ website_sale_hide_add_to_cart/README.md
@@ -0,0 +1,8 @@
+# website_sale_hide_add_to_cart
+Hide Add to Cart in Product
+
+# Before
+<img src="website_sale_hide_add_to_cart/static/description/ss_00.png">
+
+# After
+<img src="website_sale_hide_add_to_cart/static/description/ss_01.png">
---
+++ website_sale_hide_add_to_cart/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hide "Add To Cart" button product',
+    'version': '1.0',
+    'category': 'eCommerce',
+    'summary': 'Custom website product sales',
+    'author': 'Muhammad Syarif',
+    'email': 'mhdsyarif.ms@gmail.com',
+    'website': 'http://wwww.mhdsyarif.com',
+    'maintainer': 'Riau Programmer',
+    'license': 'AGPL-3',
+    'depends': ['web'],
+    'data': [
+        'views/template_product.xml',
+    ],
+    'images': ['static/description/syarif.png'],
+    'installable': True,
+    'auto_install': False
+}
