38e56e8fb8d79868ca64752b8d5d66c900fff62f
mhdsyarif <mhdsyarif.ms@gmail.com>
2018-07-24 19:48:22 +0700

    Rename depends account

--- account_report_invoice_barcode/__manifest__.py
+++ account_report_invoice_barcode/__manifest__.py
@@ -11,7 +11,7 @@
     'maintainer': 'Riau Programmer',
     'description': 'Add Barcode(Code128) on Report Invoice',
     'license': 'AGPL-3',
-    'depends': ['account','account_accountant'],
+    'depends': ['account','account_invoicing'],
     'data': [
         'views/account_invoice_form.xml',
         'report/report_invoice.xml',
