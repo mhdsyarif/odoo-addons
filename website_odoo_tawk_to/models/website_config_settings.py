73bef6386627de9cee2d34c907e2a985c0febd5a
mhdsyarif <mhdsyarif.ms@gmail.com>
2018-07-24 20:28:28 +0700

    Update table

--- website_odoo_tawk_to/models/website_config_settings.py
+++ website_odoo_tawk_to/models/website_config_settings.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 
 class WebsiteConfigSettings(models.TransientModel):
-    _inherit = 'website.config.settings'
+    _inherit = 'res.config.settings'
 
     tawk_siteid = fields.Char(
         related=['website_id', 'tawk_siteid'])
