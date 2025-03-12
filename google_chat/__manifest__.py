b77494e4ecfb531f709b07501c3b10d8119bda49
Muhammad Syarif <mhdsyarif.ms@gmail.com>
2025-03-12 22:44:44 +0700

    [ADD] License

--- google_chat/__manifest__.py
+++ google_chat/__manifest__.py
@@ -13,6 +13,7 @@
     # for the full list
     'category': 'Tools',
     'version': '15.0.0.0.1',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mail'],
